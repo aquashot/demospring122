a814961d128c0b44c2948f4448e2f0c4a49ea09e
Илон <vlasovvdm@gmail.com>
2024-04-10 10:26:33 +0400

    !

---
+++ src/main/java/com/example/demo122spring/Demo122springApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo122spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo122springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo122springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo122spring/Demo122springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo122spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo122springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
